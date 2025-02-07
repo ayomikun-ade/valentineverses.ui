4cfe36e56d155a4370e3a2ce3ecde09c4f2049d6
Ayomikun <ayoadeosun10@gmail.com>
2025-02-07 10:13:46 +0100

    feat: added copyright footer

--- src/components/Copyright.jsx
+++ src/components/Copyright.jsx
@@ -1,6 +1,6 @@
 const Copyright = () => {
   return (
-    <footer className="absolute bottom-3 left-0 right-0">
+    <footer className="absolute bottom-0 left-0 right-0 bg-neutral-100 mx-auto w-fit px-3 py-1 rounded-t-md">
       <p className="text-center font-hard text-neutral-800">
         Made with ❤️ from{" "}
         <a
