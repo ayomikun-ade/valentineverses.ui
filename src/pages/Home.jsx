3657014e99983246e4f65ff802f3a8d574fec11a
Ayomikun <ayoadeosun10@gmail.com>
2025-02-07 15:26:30 +0100

    fix: fixed backdrop brightness on home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,13 +5,13 @@ import Header from "../components/Header";
 const Home = () => {
   return (
     <>
-      <section className="relative font-soft bg-[url('/src/assets/letters.jpg')] bg-no-repeat bg-right-top bg-cover md:bg-center min-h-screen w-full flex flex-col justify-center items-center">
+      <section className="relative font-soft bg-[url('/src/assets/letters.jpg')] bg-no-repeat bg-cover bg-center min-h-screen w-full flex flex-col justify-center items-center">
         <Header />
-        <div className="backdrop-brightness-125 md:backdrop-brightness-95 backdrop-blur flex flex-col mx-3 max-w-[600px] min-h-[500px] px-6 py-8 justify-evenly items-center bg-transparent shadow-md rounded-lg">
-          <h2 className="font-hard font-bold text-center text-pink-300 md:text-pink-600 text-3xl">
+        <div className="backdrop-brightness-95 backdrop-blur flex flex-col mx-3 max-w-[600px] min-h-[500px] px-6 py-8 justify-evenly items-center bg-transparent shadow-md rounded-lg">
+          <h2 className="font-hard font-bold text-center text-pink-500 text-3xl">
             Speak from the Heart
           </h2>
-          <p className="text-center font-medium">
+          <p className="text-center font-medium text-neutral-900">
             The art of love is a timeless and universal language. <br />
             Expressing your love and affection can be nerve-wracking, but it
             doesn&apos;t have to be. Our love letter and poem generator is here
@@ -22,7 +22,7 @@ const Home = () => {
           <div className="flex gap-3 text-center my-2">
             <Link
               to="/love-letter"
-              className="bg-pink-700 text-white rounded-md px-3 py-2 transition duration-300 hover:ease-in-out hover:bg-pink-600"
+              className="bg-pink-600 text-white rounded-md px-3 py-2 transition duration-300 hover:ease-in-out hover:bg-pink-500"
             >
               Love Letter Generator
             </Link>
