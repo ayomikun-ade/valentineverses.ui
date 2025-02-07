9ce5deb42e08fec42a3a7066bb9e056fa1cd810d
Ayomikun <ayoadeosun10@gmail.com>
2025-02-07 09:50:19 +0100

    fix: changes background image

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,8 @@
 const Header = () => {
   return (
-    <div className="absolute font-logo font-medium text-4xl text-pink-600 top-0 left-0 right-0 bg-white px-10 py-3">
+    <header className="absolute font-logo font-semibold text-5xl text-white top-0 left-0 right-0 bg px-5 md:px-10 py-3">
       <p>ValentineVerses</p>
-    </div>
+    </header>
   );
 };
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,8 +5,8 @@ const Home = () => {
   return (
     <>
       <Header />
-      <section className="font-soft bg-[url('/src/assets/header.png')] bg-no-repeat bg-cover min-h-screen w-full flex flex-col justify-center items-center">
-        <div className="bg-opacity-50 backdrop-blur flex flex-col max-w-[600px] min-h-[500px] px-6 py-8 justify-evenly items-center bg-white rounded-lg">
+      <section className="font-soft bg-[url('/src/assets/letters.jpg')] bg-no-repeat bg-right-top bg-cover md:bg-center min-h-screen w-full flex flex-col justify-center items-center">
+        <div className="backdrop-brightness-95 backdrop-blur flex flex-col max-w-[600px] min-h-[500px] px-6 py-8 justify-evenly items-center bg-transparent shadow-md rounded-lg">
           <h2 className="font-hard font-semibold text-pink-600 text-3xl">
             Hello
           </h2>
@@ -19,7 +19,7 @@ const Home = () => {
             <button className="bg-pink-600 text-white rounded-md px-3 py-2 transition duration-300 hover:ease-in-out hover:bg-pink-500">
               Love Letter Generator
             </button>
-            <button className="bg-pink-200 text-black rounded-md px-3 py-2 transition duration-300 hover:ease-in-out hover:bg-pink-100">
+            <button className="bg-pink-200 text-black rounded-md px-3 py-2 transition duration-300 hover:ease-in-out hover:bg-pink-300">
               Poem Generator
             </button>
           </div>
