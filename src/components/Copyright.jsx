23a0bd7a4915d65c7ff5267dbd03b9086460a45a
Ayomikun <ayoadeosun10@gmail.com>
2025-02-09 11:49:56 +0100

    fix: fixed copyright positioning

--- src/components/Copyright.jsx
+++ src/components/Copyright.jsx
@@ -1,8 +1,8 @@
 const Copyright = () => {
   return (
-    <footer className="absolute bottom-0 left-0 right-0 bg-neutral-100 mx-auto w-fit px-3 py-1 mb-0 rounded-t-md">
+    <footer className="fixed bottom-0 left-0 right-0 bg-neutral-100 mx-auto w-fit px-3 py-1 mb-0 rounded-t-md">
       <p className="text-center font-hard text-neutral-800 mb-0">
-        Made with ❤️ from{" "}
+        Made with ❤️ by{" "}
         <a
           href="https://www.github.com/ayomikun-ade"
           target="_blank"
