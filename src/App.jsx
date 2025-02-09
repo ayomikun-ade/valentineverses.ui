b98097213d208a020861321e6c24305c60971283
Ayomikun <ayoadeosun10@gmail.com>
2025-02-09 11:51:07 +0100

    feat: added background music

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,20 @@ import { BrowserRouter, Route, Routes } from "react-router";
 import Home from "./pages/Home";
 import LoveLetter from "./pages/LoveLetter";
 import PoemGen from "./pages/PoemGen";
+import Header from "./components/Header";
+import Copyright from "./components/Copyright";
 
 const App = () => {
   return (
     <>
       <BrowserRouter>
+        <Header />
         <Routes>
           <Route index element={<Home />} />
           <Route path="/love-letter" element={<LoveLetter />} />
           <Route path="/poem-generator" element={<PoemGen />} />
         </Routes>
+        <Copyright />
       </BrowserRouter>
     </>
   );
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,39 @@
+import { useEffect, useState } from "react";
 import { Link } from "react-router";
 
 const Header = () => {
+  const [musicPlay, setMusicPlay] = useState(false);
+
+  const handleMusicToggle = () => {
+    setMusicPlay(!musicPlay);
+  };
+
+  useEffect(() => {
+    const audio = document.getElementById("background-music");
+    if (musicPlay) {
+      audio.play();
+    } else {
+      audio.pause();
+    }
+  }, [musicPlay]);
+
   return (
-    <header className="absolute font-logo font-semibold text-5xl text-center md:text-left text-white top-0 left-0 right-0 bg px-5 md:px-10 py-4">
+    <header className="fixed font-logo font-semibold backdrop-blur z-20 text-5xl flex justify-center md:justify-between items-center text-center md:text-left text-white top-0 left-0 right-0 bg px-5 md:px-10 py-4">
       <Link to="/" className="outline-none">
         ValentineVerses
       </Link>
+      <button
+        onClick={handleMusicToggle}
+        className="font-soft text-sm text-end absolute right-2 md:right-10"
+      >
+        Music
+      </button>
+
+      {/* background music */}
+      <audio id="background-music" loop>
+        <source src="/src/assets/val-audio.mp3" type="audio/mp3" />
+        Your browser does not support the audio element.
+      </audio>
     </header>
   );
 };
